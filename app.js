8d57047afdab8c232951c424cd2413441bcc2907
arvin lorenz <arvinlorenz@gmail.com>
2021-06-03 14:42:47 +0800

    80

--- app.js
+++ app.js
@@ -10,6 +10,20 @@ app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
 app.use(bodyParser.json())
+
+app.use((req, res, next) => {
+  res.setHeader('X-Auth-Token', '*')
+  res.setHeader('Access-Control-Allow-Origin', '*')
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin,X-Requested-With, Content-Type,Accept,Authorization'
+  )
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE, OPTIONS'
+  )
+  next()
+})
 app.get('/htmlData', (req, res) => {
   try {
     const data = fs.readFileSync(
