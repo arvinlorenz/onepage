2716d6ea959ca3f15b1028cfb4b23a4e0d266830
arvin lorenz <arvinlorenz@gmail.com>
2021-06-03 14:24:09 +0800

    port 80

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const fs = require('fs')
 const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
-const port = process.env.PORT || '3000'
+const port = process.env.PORT || '80'
 
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
